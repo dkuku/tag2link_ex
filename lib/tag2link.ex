f1358b80e8e6c3dcecf8773aa82c3248c824f208
Daniel Kukula <daniel.kuku@gmail.com>
2024-06-22 00:01:17 +0100

    update license to same as upstream

--- lib/tag2link.ex
+++ lib/tag2link.ex
@@ -3,14 +3,14 @@ defmodule Tag2link do
   Documentation for `Tag2link`.
   """
   @mapping :tag2link
-    |> Application.app_dir()
-    |> Kernel.<>("/priv/index.json")
-    |> File.read!()
-    |> Jason.decode!(keys: :atoms)
-    |> Enum.group_by(& &1[:key])
-    |> Enum.map(fn {_key, group} ->
-      Enum.find(group, & &1[:rank] == "preferred") || hd(group)
-    end)
+           |> Application.app_dir()
+           |> Kernel.<>("/priv/index.json")
+           |> File.read!()
+           |> Jason.decode!(keys: :atoms)
+           |> Enum.group_by(& &1[:key])
+           |> Enum.map(fn {_key, group} ->
+             Enum.find(group, &(&1[:rank] == "preferred")) || hd(group)
+           end)
 
   for %{key: "Key:" <> key, url: url_template} <- @mapping do
     def to_url({unquote(key), value}) do
--- mix.exs
+++ mix.exs
@@ -32,8 +32,11 @@ defmodule Tag2link.MixProject do
     [
       files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
                 license* CHANGELOG* changelog* src),
-      licenses: ["Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/dkuku/tag2link_ex"}
+      licenses: ["ISC"],
+      links: %{
+        "GitHub" => "https://github.com/dkuku/tag2link_ex",
+        "Original project" => "https://github.com/JOSM/tag2link"
+      }
     ]
   end
 end
